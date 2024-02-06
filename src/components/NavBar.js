6222f1eb24bd031963a2147caa93f64c7c728442
BakaUSPSB <cjs5@njit.edu>
2024-02-05 19:34:13 -0500

    added navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,49 @@
-import { Navbar, Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
 export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Actors</Nav.Link>
-            <Nav.Link href="#link">Movies</Nav.Link>
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#link"
+              className={
+                activeLink === "films" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("films")}
+            >
+              Films
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
